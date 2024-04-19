980b589fa015be4a8e4302dd37d292849d988187
Asanapuram Venugopal <116752027+AsanapuramVenugopal@users.noreply.github.com>
2024-04-20 02:32:21 +0530

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,71 @@
+// ==========================================toggle icon navbar=====================================
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('fa-xmark');
+  navbar.classList.toggle('active');
+}
+
+
+
+// ========================================== Scroll section active link =====================================
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
+window.onscroll = () => {
+  sections.forEach (sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach.apply(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
+      });
+    };
+  });
+
+  // ==========================================sticky navbar=====================================
+  let header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 100);
+
+  // ==========================================remove toggle icon and navbar=====================================
+
+  menuIcon.classList.remove('fa-xmark');
+  navbar.classList.remove('active');
+};
+
+// ==========================================scroll reveal =====================================
+
+
+ScrollReveal({
+  distance:'80px',
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form',{ origin: 'buttom' });
+ScrollReveal().reveal('.home-contact h1, .about-img',{ origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
+
+
+
+
+// ==========================================Typed js =====================================
+
+
+const typed = new Typed('.multiple-text',{
+  strings:['Python Engineer', 'Data Analyst', 'SQL Developer', 'Frontend Developer', 'Backend Developer'],
+  typeSpeed : 80,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true,
+});
